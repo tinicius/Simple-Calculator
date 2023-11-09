3428f7a96a67c4768a85b6cb36011e08413f8591
César Henrique Resende Soares <73477344+CesarHRS@users.noreply.github.com>
2023-11-09 15:09:13 -0300

    Update main.js
    Falta implementar o M+ e o M-

--- src/main.js
+++ src/main.js
@@ -1,10 +1,55 @@
 
 let operation = ""
+let memory = ""
 
 const setOperation = (value) => {
     operation += value
 }
+const clearOperation = () =>{
+    operation = ""
+}
 
+const plusOperation = () =>{
+    operation += "+"
+}
+
+const minusOperation = () =>{
+    operation += "-"
+}
+const divisionOperation = () =>{
+    operation += "/"
+}
+
+const multiplicationOperation = () =>{
+    operation += "*"
+}
+
+const changeSignalOperation = () =>{
+    operation += "* (-1)"
+}
+
+const setComma = () =>{
+    operation += ","
+}
 const calculate = () => {
-    alert(operation)
+    //alert(operation)
+    alert(eval(operation))
+}
+const memoryRead = () => {
+    alert(memory)
+}
+
+const memoryClear = () => {
+    memory = ""
+}
+
+const memorySave = () => {
+    memory = eval(operation)
+}
+
+const memoryAdd = () =>{
+
+}
+const memoryRemove = () =>{
+
 }
